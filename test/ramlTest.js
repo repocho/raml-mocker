e8da2e87d24a0012710de5863c3c8d1a2f23909e
David Vega <david@zonamail.com>
2014-07-25 01:57:52 +0200

    Updates the tests to the new functionlallity

--- test/ramlTest.js
+++ test/ramlTest.js
@@ -1,7 +1,11 @@
 'use strict';
+var _ = require('lodash');
 var ramlMocker = require('../src/index.js');
 ramlMocker.generate({
     path: 'test/raml'
 }, function (methods) {
     console.log(methods);
+    _.each(methods, function (m) {
+        console.log(m.mock());
+    });
 });
