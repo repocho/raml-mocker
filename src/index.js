700b0efaca88fe52853957679ff15601f1939589
David Vega <david@zonamail.com>
2014-07-28 08:35:36 +0200

    Adds generate from Files. Changes the return to array. Changes in some names. Adds some errors detections.

--- src/index.js
+++ src/index.js
@@ -8,12 +8,39 @@ var path = require('path'),
 
 function generate(options, callback) {
     var formats = {};
-    if (options.formats) {
-        formats = options.formats;
+    if (options) {
+        if (options.formats) {
+            formats = options.formats;
+        }
+        if (!callback || !_.isFunction(callback)) {
+            console.error('[RAML-MOCKER] You must define a callback function:\n');
+            showUsage();
+        }
+        try {
+            if (options.path) {
+                generateFromPath(options.path, formats, callback);
+            } else if (options.files && _.isArray(options.files)) {
+                generateFromFiles(options.files, formats, callback);
+            }
+        } catch (exception) {
+            console.error('[RAML-MOCKER] A runtime error has ocurred:\n');
+            console.error(exception.stack);
+            showUsage();
+        }
+    } else {
+        console.error('[RAML-MOCKER] You must define a options object:\n');
+        showUsage();
     }
-    if (options && options.path) {
-        generateFromPath(options.path, formats, callback);
-    }
+}
+
+function showUsage() {
+    console.log('--------------------------------------------------------------------');
+    console.log('---------------------- HOW TO USE RAML MOCKER ----------------------');
+    console.log('--  var ramlMocker = require(\'raml-mocker\');                      --');
+    console.log('--  var options = { path: \'test/raml\' };                          --');
+    console.log('--  var callback = function (requests){ console.log(requests); }; --');
+    console.log('--  ramlMocker.generate(options, callback);                       --');
+    console.log('--------------------------------------------------------------------');
 }
 
 function generateFromPath(filesPath, formats, callback) {
@@ -21,30 +48,37 @@ function generateFromPath(filesPath, formats, callback) {
         if (err) {
             throw err;
         }
-        files = files.filter(function (file) {
-            return file.substr(-5) === '.raml';
-        });
-        var methods = {};
-        async.each(files, function (file, cb) {
-            raml.loadFile(path.join(filesPath, file)).then(function (data) {
-                var m = getRamlMethods(data, '/');
-                methods = _.extend(methods, m);
-                cb();
-            }, function (error) {
-                cb('Error parsing: ' + error);
-            });
-        }, function (err) {
-            if (err) {
-                console.log(err);
-            } else {
-                callback(methods);
+        var filesToGenerate = [];
+        _.each(files, function (file) {
+            if (file.substr(-5) === '.raml') {
+                filesToGenerate.push(path.join(filesPath, file));
             }
         });
+        generateFromFiles(filesToGenerate, formats, callback);
     });
 }
 
-function getRamlMethods(definition, uri, formats) {
-    var methods = {};
+function generateFromFiles(files, formats, callback) {
+    var requestsToMock = [];
+    async.each(files, function (file, cb) {
+        raml.loadFile(file).then(function (data) {
+            var m = getRamlRequestsToMock(data, '/');
+            requestsToMock = _.union(requestsToMock, m);
+            cb();
+        }, function (error) {
+            cb('Error parsing: ' + error);
+        });
+    }, function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            callback(requestsToMock);
+        }
+    });
+}
+
+function getRamlRequestsToMock(definition, uri, formats) {
+    var requestsToMock = [];
     if (definition.relativeUri) {
         var nodeURI = definition.relativeUri;
         if (definition.uriParameters) {
@@ -59,12 +93,13 @@ function getRamlMethods(definition, uri, formats) {
             if (method.method && /get|post|put|delete/i.test(method.method) && method.responses && method.responses[200] && method.responses[200].body && method.responses[200].body['application/json'] && method.responses[200].body['application/json'].schema) {
                 try {
                     var schema = JSON.parse(method.responses[200].body['application/json'].schema);
-                    methods[uri] = {
+                    requestsToMock.push({
+                        uri: uri,
                         method: method.method,
                         mock: function () {
                             return schemaMocker(schema, formats);
                         }
-                    };
+                    });
                 } catch (exception) {
                     console.log(exception.stack);
                 }
@@ -73,10 +108,10 @@ function getRamlMethods(definition, uri, formats) {
     }
     if (definition.resources) {
         _.each(definition.resources, function (def) {
-            methods = _.extend(methods, getRamlMethods(def, uri));
+            requestsToMock = _.union(requestsToMock, getRamlRequestsToMock(def, uri));
         });
     }
-    return methods;
+    return requestsToMock;
 }
 module.exports = {
     generate: generate
